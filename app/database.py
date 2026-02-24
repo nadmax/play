6324d3fc5da5487368cd20d3f7398c7d29446697
nadmax <maximilien.nadji@pm.me>
2026-02-24 14:52:12 +0100

    refactor: extract constants into dedicated const module

--- app/database.py
+++ app/database.py
@@ -1,14 +1,6 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-
-user = os.environ["POSTGRES_USER"]
-password = os.environ["POSTGRES_PASSWORD"]
-db = os.environ["POSTGRES_DB"]
-host = os.environ["POSTGRES_HOST"]
-port = os.environ["POSTGRES_PORT"]
-
-DATABASE_URL = f"postgresql://{user}:{password}@{host}/{db}:{port}"
+from .const import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
